d7734ba189825442e7d7ad8f77b0821813add13b
MetiH <38054818+MetiH@users.noreply.github.com>
2018-04-21 08:56:15 -0700

    HW4

---
+++ CoffeeShop/Models/CoffeeRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Models
+{
+    public class CoffeeRepository: ICoffeeRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CoffeeRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Coffee> GetAllCoffees()
+        {
+            return _appDbContext.coffees;
+        }
+
+        public IEnumerable<Coffee> GetAllPies()
+        {
+            return _appDbContext.coffees;
+        }
+
+        public Coffee GetCoffeeById(int coffeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Coffee GetPieById(int coffeeId)
+        {
+            return _appDbContext.coffees.FirstOrDefault(p => p.Id == coffeeId);
+        }
+    }
+}
